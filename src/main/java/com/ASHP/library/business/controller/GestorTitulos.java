7ad0d2b74018a483bbf0c7c901b256d7799c4b90
Alonso <81324208+Alonso256@users.noreply.github.com>
2023-10-23 20:05:34 +0200

    Revert "Implementada funcionalidad Registrar Titulo"
    This reverts commit 0cd2b7860aa6c7cfa92455ae0153e1e73a539896.

--- src/main/java/com/ASHP/library/business/controller/GestorTitulos.java
+++ src/main/java/com/ASHP/library/business/controller/GestorTitulos.java
@@ -1,32 +1,17 @@
 package com.ASHP.library.business.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import com.ASHP.library.business.entity.Titulo;
 import com.ASHP.library.business.persistence.AutorDAO;
 import com.ASHP.library.business.persistence.EjemplarDAO;
 import com.ASHP.library.business.persistence.TituloDAO;
 
-@Controller
 public class GestorTitulos {
 	public TituloDAO _tituloDAO;
 	public EjemplarDAO _ejemplarDAO;
 	public AutorDAO _autorDAO;
 
-	@PostMapping("/titulo")
-	public String altaTitulo(@ModelAttribute Titulo titulo, Model model) {
-		model.addAttribute("titulo", titulo);
-		return "vista-titulo";
-	}
-	
-	@GetMapping("/titulo")
-	public String verTitulo(Model model) {
-	model.addAttribute("titulo", new Titulo());
-	return "titulo";
+	public Titulo altaTitulo(String aTitulo, String aIsbn, String[] aAutores) {
+		throw new UnsupportedOperationException();
 	}
 
 	public void actualizarTitulo(Titulo aT) {
