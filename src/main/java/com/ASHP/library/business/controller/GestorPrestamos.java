f4338efcebe73c7361110280e1a6ca4b40c9a55c
Piedad1 <91836109+Piedad1@users.noreply.github.com>
2023-12-01 10:20:47 +0100

    Controllers modificados

--- src/main/java/com/ASHP/library/business/controller/GestorPrestamos.java
+++ src/main/java/com/ASHP/library/business/controller/GestorPrestamos.java
@@ -1,6 +1,10 @@
 package com.ASHP.library.business.controller;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,14 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ASHP.library.business.entity.Ejemplar;
+import com.ASHP.library.business.entity.Prestamo;
 import com.ASHP.library.business.entity.Titulo;
 import com.ASHP.library.business.entity.Usuario;
+import com.ASHP.library.business.persistence.EjemplarDAO;
 import com.ASHP.library.business.persistence.PrestamoDAO;
 import com.ASHP.library.business.persistence.ReservaDAO;
 import com.ASHP.library.business.persistence.TituloDAO;
 import com.ASHP.library.business.persistence.UsuarioDAO;
-
 
 @Controller
 public class GestorPrestamos {
@@ -28,25 +36,104 @@ public class GestorPrestamos {
 	@Autowired
 	public TituloDAO _tituloDAO;
 	@Autowired
+	public EjemplarDAO _ejemplarDAO;
+
+	@Autowired
 	public UsuarioDAO _usuarioDAO;
-	
+
 	@GetMapping("/prestamo")
 	public String menuPrestamos() { return "prestamo"; }
-	
+
 	@GetMapping("/nuevo-prestamo")
 	public String menuNuevoPrestamo(Model model) {
 		List<Titulo> todosLosTitulos = _tituloDAO.findAll();
 		List<Usuario> todosLosUsuarios = _usuarioDAO.findAll();
-
 		model.addAttribute("titulos", todosLosTitulos);
 		model.addAttribute("usuarios", todosLosUsuarios);
 
 		return "nuevo-prestamo";
 	}
 
+	@PostMapping("/nuevo-prestamo")
+	public String postNuevoPrestamo(
+		@RequestParam Long usuario, 
+		@RequestParam Long titulo, 
+		Model model) {
+		// Obtener el título por su ID
+		Optional<Titulo> tOptional = _tituloDAO.findById(titulo);
+		Titulo t = tOptional.get();
 
-	public void realizarPrestamo(String aIsbn, String aIdEjemplar, String aIdUsuario) {
-		throw new UnsupportedOperationException();
+		// Obtener el usuario por su ID
+		Optional<Usuario> uOptional = _usuarioDAO.findById(usuario);
+		Usuario u = uOptional.get();
+
+		ArrayList<String> errores = new ArrayList();
+
+		// FIXME: establecer ese 4 de alguna propiedad MAX_PRESTAMOS
+		// de alguna constante, algún fichero de config, vars. de entorno...
+		if ( u.getPrestamos().size() >= 4 ) {
+			errores.add("Usuario supera el límite de préstamos (4)");
+		// TODO: comparar esta fecha con el día de hoy.
+		} else if ( u.getFechaFinPenalizacion() != null ) {
+			errores.add("Usuario penalizado. No se le puede prestar más hasta: " + 
+							u.getFechaFinPenalizacion());
+		}
+
+		List<Ejemplar> ejemplares = _ejemplarDAO.findEjemplaresDisponibles(t);
+		System.out.println(ejemplares);
+		if ( ejemplares.size() == 0 ) {
+			errores.add("No hay ejemplares disponibles");
+		}
+
+
+		if (errores.size() > 0) {
+			List<Titulo> todosLosTitulos = _tituloDAO.findAll();
+			List<Usuario> todosLosUsuarios = _usuarioDAO.findAll();
+			model.addAttribute("titulos", todosLosTitulos);
+			model.addAttribute("usuarios", todosLosUsuarios);
+
+			model.addAttribute("errores", errores);
+			model.addAttribute("seleccionado", false);
+		} else {
+			model.addAttribute("seleccionado", true);
+		}
+		
+		model.addAttribute("errores", errores);
+		model.addAttribute("ejemplares", ejemplares);
+		model.addAttribute("titulo", t);
+		model.addAttribute("usuario", u);
+
+		return "nuevo-prestamo";
+	}
+
+	@PostMapping("/crear-prestamo")
+	public String realizarPrestamo(
+		@RequestParam Long usuario, 
+		@RequestParam Long titulo,
+		@RequestParam Long ejemplar) {
+		// Obtener el título por su ID
+		Optional<Titulo> tOptional = _tituloDAO.findById(titulo);
+		Titulo t = tOptional.get();
+
+		// Obtener el usuario por su ID
+		Optional<Usuario> uOptional = _usuarioDAO.findById(usuario);
+		Usuario u = uOptional.get();
+
+		// Obtener el ejemplar por su ID
+		Optional<Ejemplar> eOptional = _ejemplarDAO.findById(ejemplar);
+		Ejemplar e = eOptional.get();
+		
+		// Obtener fechas de inicio y de fin
+		Date fechaInicio = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fechaInicio);
+        calendar.add(Calendar.DAY_OF_YEAR, 7);
+        Date fechaFin = calendar.getTime();
+
+		Prestamo p = new Prestamo(fechaInicio, fechaFin, true, u, t, e);
+		_prestamoDAO.save(p);
+
+		return "confirmacion-prestamo";
 	}
 
 	public void realizarDevolucion(String aIsbn, String aIdEjemplar, String aIdUsuario) {
--- src/main/java/com/ASHP/library/business/controller/GestorTitulos.java
+++ src/main/java/com/ASHP/library/business/controller/GestorTitulos.java
@@ -1,7 +1,7 @@
 package com.ASHP.library.business.controller;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,20 +12,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.ASHP.library.business.entity.Titulo;
 import com.ASHP.library.business.entity.Autor;
 import com.ASHP.library.business.entity.Ejemplar;
+import com.ASHP.library.business.entity.Titulo;
 import com.ASHP.library.business.persistence.AutorDAO;
-import com.ASHP.library.business.persistence.EjemplarDAO;
 import com.ASHP.library.business.persistence.TituloDAO;
-import com.ASHP.library.business.servicios.TituloServices;
 
-@RestController
+@Controller
 public class GestorTitulos {
 
 	private static final Logger log = LoggerFactory.getLogger(GestorTitulos.class);
@@ -34,44 +29,47 @@ public class GestorTitulos {
 	public TituloDAO tituloDAO;
 //	@Autowired
 //	public EjemplarDAO _ejemplarDAO;
-	@Autowired
-	public AutorDAO autorDAO;
-	
+//	@Autowired
+//	public AutorDAO _autorDAO;
+
 	// @Autowired
 	// private TituloServices tituloService;
 
+	@Autowired
+	private AutorDAO autorDAO;
+
 	public GestorTitulos(TituloDAO tituloDAO, AutorDAO autorDAO) {
+
 		super();
 		this.tituloDAO = tituloDAO;
+
 		this.autorDAO = autorDAO;
-		
+
 	}
 
-	
-
-	@PostMapping("/altatitulo")
+	@PostMapping("/altaTitulo")
 	public String altaTitulo(@RequestParam List<String> titulo, @RequestParam List<String> autor, Model model) {
-		
+
 		String nombreTitulo, numReserva, isbn, nombreAutor, apellidoAutor;
-		
+
 		nombreTitulo = titulo.get(0);
 		isbn = titulo.get(1);
 		numReserva = titulo.get(2);
-		
+
 		Titulo t = new Titulo(nombreTitulo, isbn, numReserva);
 		Ejemplar e = new Ejemplar(t);
-		
+
 		nombreAutor = autor.get(0);
 		apellidoAutor = autor.get(1);
 		List<Titulo> titulos = new ArrayList<Titulo>();
 		titulos.add(t);
 		Autor a = new Autor(nombreAutor, apellidoAutor, titulos);
-		
+
 		model.addAttribute("titulo", titulo);
 		//tituloDAO.save(titulo);
 		return "vista-titulo";
 	}
-	
+
 	@GetMapping("/vistaFormTituloAutor")
 	public String verFormulario(Model model) {
 		Titulo titulo = new Titulo();
@@ -84,8 +82,9 @@ public class GestorTitulos {
 		return "titulo";
 	}
 
+
 	@GetMapping("/titulos")
-	public String ListarTitulos(Model model) {
+	public String listarTitulos(Model model) {
 		List<Titulo> todosLosTitulos = tituloDAO.findAll();
 
 		model.addAttribute("titulos", todosLosTitulos);
