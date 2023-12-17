eddfed3099a86298bacdd82a186bd138149682c2
Piedad1 <91836109+Piedad1@users.noreply.github.com>
2023-12-17 06:35:51 +0100

    Gestor de penalizaciones completado

--- src/main/java/com/ASHP/library/business/controller/GestorPenalizaciones.java
+++ src/main/java/com/ASHP/library/business/controller/GestorPenalizaciones.java
@@ -1,10 +1,14 @@
 package com.ASHP.library.business.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.ASHP.library.business.entity.Prestamo;
 import com.ASHP.library.business.entity.Usuario;
 import com.ASHP.library.business.persistence.UsuarioDAO;
 
@@ -15,11 +19,23 @@ public class GestorPenalizaciones {
 	@Autowired
 	public UsuarioDAO _usuarioDAO;
 
-	public void aplicarPenalizacion(Usuario aU) {
-		throw new UnsupportedOperationException();
+	public void aplicarPenalizacion(Usuario usuario) {
+		Date fechaActual = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(fechaActual);
+		calendar.add(Calendar.DAY_OF_YEAR, 30); // 30 días desde hoy
+		Date fechaPenalizacion = calendar.getTime();
+		
+		usuario.setFechaFinPenalizacion(fechaPenalizacion);
+		_usuarioDAO.save(usuario);
 	}
-
-	public void comprobarPenalizacion(Usuario aU) {
-		throw new UnsupportedOperationException();
+	
+	/**
+	 * @param usuario usuario a comprobar
+	 * @return true si está penalizado, false si no.
+	 */
+	public boolean comprobarPenalizacion(Usuario usuario) {
+		return usuario.getFechaFinPenalizacion() != null 
+				&& usuario.getFechaFinPenalizacion().compareTo(new Date()) > 0;
 	}
 }
