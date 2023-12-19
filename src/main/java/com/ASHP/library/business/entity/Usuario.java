c691cfce9b06e0569fb3a5f9dfd80bf9798dd3fb
Sara <109819067+sxrxsr@users.noreply.github.com>
2023-12-19 19:20:05 +0100

    Arreglado los fallos

--- src/main/java/com/ASHP/library/business/entity/Usuario.java
+++ src/main/java/com/ASHP/library/business/entity/Usuario.java
@@ -10,34 +10,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
+@Table(name = "usuario")
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column
 	private String nombre;
+	
 	@Column
 	private String apellidos;
+	
 	@Column
 	private Date fechaFinPenalizacion;
 		
-    @OneToMany
+    @OneToMany(mappedBy = "usuario")
 	public List<Prestamo> prestamos = new ArrayList<Prestamo>();
     
-    @OneToMany
+    @OneToMany(mappedBy = "usuario")
 	public List<Reserva> reservas = new ArrayList<Reserva>();
 
 	public Usuario() {
 	}
 
-	public Usuario(String nombre, String apellidos) {
-		this(nombre, apellidos, null, List.of(), List.of());
-	}
-
-	public Usuario(String nombre, String apellidos, Date fechaFinPenalizacion,
+	public Usuario(String nombre, String apellidos, Date fechaFinPenalizacion, Object attribute,
 			List<Prestamo> prestamos, List<Reserva> reservas) {
 		super();
 		this.nombre = nombre;
